4004d92b4e170e578f6afcfae978989a22a5692d
Arturo Diaz <luisdiazuceda@outlook.com>
2026-08-06 20:55:15 -0600

    Bug db Railway

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
